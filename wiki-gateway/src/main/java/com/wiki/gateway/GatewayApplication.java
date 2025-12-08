359ae6f0260b61ad3e205bcecaad78f26181c971
vcheb <slavacheban22@gmail.com>
2025-12-08 19:15:56 +0200

    fix start up via skaffold

--- wiki-gateway/src/main/java/com/wiki/gateway/GatewayApplication.java
+++ wiki-gateway/src/main/java/com/wiki/gateway/GatewayApplication.java
@@ -2,8 +2,15 @@ package com.wiki.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(
+        exclude = {
+                SecurityAutoConfiguration.class,
+                UserDetailsServiceAutoConfiguration.class
+        }
+)
 public class GatewayApplication {
 
     public static void main(String[] args) {
